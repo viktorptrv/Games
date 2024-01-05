2112fea0b280f261554cb6c5528444047141c59b
Viktor Petrov <115278686+viktorptrv@users.noreply.github.com>
2024-01-05 20:18:39 +0200

    Add files via upload

---
+++ QRCodeGenerator.py
@@ -0,0 +1,27 @@
+import qrcode
+
+
+class MyQR():
+    def __init__(self, size: int, padding: int):
+        self.qr = qrcode.QRCode(box_size=size, border=padding)
+
+    def create_qr(self, file_name: str, fg: str, bg: str):
+        user_input = str(input("Enter text: "))
+        try:
+            self.qr.add_data(user_input)
+            qr_image = self.qr.make_image(fill_color = fg, back_color = bg)
+            qr_image.save(file_name)
+
+            print(f"Successfully created! {file_name}")
+
+        except Exception as ex:
+            print(f"Error: {e}")
+
+
+def main():
+    myqr = MyQR(size=30, padding=2)
+    myqr.create_qr("sample.png",
+                   fg ='black',
+                   bg = 'yellow')
+
+main()
